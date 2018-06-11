b72a2729f2011331ddfcc6f167d75a8f9b586c01
Evan <ewlusky@gmail.com>
2018-06-11 14:29:04 -0500

    powers practice

--- exercises/event-wrangling/powers.js
+++ exercises/event-wrangling/powers.js
@@ -8,9 +8,36 @@
 */
 
 function handlerFunction() {
-    console.log(event.target)
+    const id = (event.target.id).split('-')
+    if (id[0] == 'activate') {
+        if (id[1] == 'all') {
+            const pows = document.querySelectorAll('.power')
+            pows.forEach(pow => {
+                console.log(pow)
+                pow.classList.add('enabled')
+                pow.classList.remove('disabled')
+            })
+        } else {
+            const tar = document.querySelector(`#${id[1]}`)
+            tar.classList.add('enabled')
+            tar.classList.remove('disabled')
+        }
+    } else {
+        const depows = document.querySelectorAll('.power')
+        depows.forEach(depow => {
+            console.log(depow)
+            depow.classList.remove('enabled')
+            depow.classList.add('disabled')
+        })
+        
+    }
+    console.log(event.target.id)
 }
 document.querySelector("#activate-flight").addEventListener('click', handlerFunction)
+document.querySelector("#activate-mindreading").addEventListener('click', handlerFunction)
+document.querySelector("#activate-xray").addEventListener('click', handlerFunction)
+document.querySelector("#activate-all").addEventListener('click', handlerFunction)
+document.querySelector("#deactivate-all").addEventListener('click', handlerFunction)
 
 /*
     Now write two more event handlers that activate the other two powers
